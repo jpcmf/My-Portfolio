f37e73a7bddb10954829129ffd90e8d6c13ea278
Joao Paulo <jpfricks@gmail.com>
2016-08-30 02:40:58 -0300

    fix info home, melhorias html/css, subindo versao

--- home.php
+++ home.php
@@ -1,19 +1,31 @@
 <div class="content-all">
     <div id="content" class="content-home">
         <h1>
-            <span class="s-title">João Paulo Fricks</span>
-            <span class="s-desc">Designer e Desenvolvedor Front End</span>
-            <!-- <span class="s-desc">Designer com uma paixão por design e experiências de usuário funcionais.<br>Desenvolvedor Front End concentrado em escrever código limpo, elegante e eficiente</span> -->
+            <span class="s-title hidden-xs">João Paulo Fricks</span>
         </h1>
-        <a href="mailto:jpfricks@gmail.com?subject=Olá João Paulo Fricks" class="btn bt-hire center-block">
+        <span class="barra"></span>
+        <h3>
+            <span class="s-desc"><u>Designer</u> com uma paixão por design e experiências de usuário funcionais.<br><u>Desenvolvedor Front End</u> concentrado em escrever código limpo, elegante e eficiente.</span>
+        </h3>
+
+        <ul class="social-list list-inline">
+            <!-- <li class=""><a href="mailto:jpfricks@gmail.com?subject=Olá João Paulo Fricks" target="_blank"><i class="fa fa-envelope-o"></i></a></li> -->
+            <li class=""><a href="//br.linkedin.com/in/joaopaulofricks" target="_blank"><i class="fa fa-linkedin-square"></i>&nbsp;LinkedIn</a></li>
+            <li class=""><a href="//twitter.com/JoaoPauloFricks" target="_blank"><i class="fa fa-twitter"></i>&nbsp; Twitter</a></li>
+            <li class=""><a href="//github.com/fricks" target="_blank"><i class="fa fa-github-alt"></i>&nbsp; Github</a></li>
+            <li class=""><a href="//pt.stackoverflow.com/users/8050/fricks" target="_blank"><i class="fa fa-stack-overflow"></i>&nbsp; StackOverflow</a></li>
+        </ul>
+
+        <a href="mailto:jpfricks@gmail.com?subject=Olá João Paulo Fricks" class="btn bt-hire">
             <i class="fa fa-paper-plane" aria-hidden="true"></i>&nbsp; Me contrate
         </a>
-        <div class="text-center group-skills">
+
+        <div class="group-skills hidden-xs">
             <span class="label label-skill">HTML</span>
             <span class="label label-skill">CSS</span>
             <span class="label label-skill">SASS</span>
             <span class="label label-skill">Stylus</span>
-            <br>
+            <!-- <br> -->
             <span class="label label-skill">JavaScript</span>
             <span class="label label-skill">PHP</span>
             <span class="label label-skill">SQL</span>
@@ -22,19 +34,14 @@
             <span class="label label-skill">Symfony</span>
             <span class="label label-skill">Laravel</span>
             <span class="label label-skill">AngularJs</span>
-            <br>
+            <!-- <br> -->
             <span class="label label-skill">Web Design</span>
             <span class="label label-skill">Design Responsive</span>
             <br>
             <span class="label label-skill">Wordpress</span>
             <span class="label label-skill">English</span>
+            <span class="label label-skill">Impressos</span>
         </div>
-        <ul class="social-list list-inline">
-            <!-- <li class=""><a href="mailto:jpfricks@gmail.com?subject=Olá João Paulo Fricks" target="_blank"><i class="fa fa-envelope-o"></i></a></li> -->
-            <li class=""><a href="//br.linkedin.com/in/joaopaulofricks" target="_blank"><i class="fa fa-linkedin-square"></i>&nbsp;LinkedIn</a></li>
-            <li class=""><a href="//twitter.com/JoaoPauloFricks" target="_blank"><i class="fa fa-twitter"></i>&nbsp; Twitter</a></li>
-            <li class=""><a href="//github.com/fricks" target="_blank"><i class="fa fa-github-alt"></i>&nbsp; Github</a></li>
-            <li class=""><a href="//pt.stackoverflow.com/users/8050/fricks" target="_blank"><i class="fa fa-stack-overflow"></i>&nbsp; StackOverflow</a></li>
-        </ul>
+
     </div>
 </div>
