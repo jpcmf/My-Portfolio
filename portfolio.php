a1a5be73a976e0e42af21ac15b7e9c7ac5f27199
João Paulo <musicjoaopaulo@gmail.com>
2018-04-02 21:43:37 -0300

    Add url to portfolio

--- portfolio.php
+++ portfolio.php
@@ -15,12 +15,12 @@
             </div>
           </div>
           <div class="work-center work-center__nopadding">
-            <a class="" href="//goo.gl/jbehXd" target="_blank" rel="noopener" title="Showcase Landing Page Neodent 2017 - Portfolio João Paulo">
+            <a class="" href="https://neodent.netlify.com" target="_blank" rel="noopener" title="Showcase Landing Page Neodent 2017 - Portfolio João Paulo">
               <img class="img-responsive lazy" width="1000" height="594" data-original="assets/images/portfolio/site-lp-neodent2017.png" alt="Showcase Landing Page Neodent 2017">
             </a>
           </div>
           <div class="work-center">
-            <a class="btn bt-enviar-contato center-block" href="//goo.gl/jbehXd" target="_blank" rel="noopener">Go</a>
+            <a class="btn bt-enviar-contato center-block" href="https://neodent.netlify.com" target="_blank" rel="noopener">Go</a>
           </div>
         </div>
       </li>
@@ -32,12 +32,12 @@
             </div>
           </div>
           <div class="work-center work-center__nopadding">
-            <a class="" href="//goo.gl/jbehXd" target="_blank" rel="noopener" title="Showcase Kibo Sushi Restaurant - Portfolio João Paulo">
+            <a class="" href="https://goo.gl/jbehXd" target="_blank" rel="noopener" title="Showcase Kibo Sushi Restaurant - Portfolio João Paulo">
               <img class="img-responsive lazy" width="1000" height="594" data-original="assets/images/portfolio/site-kibo2016.jpg" alt="Showcase Kibo Sushi Restaurant - Portfolio João Paulo">
             </a>
           </div>
           <div class="work-center">
-            <a class="btn bt-enviar-contato center-block" href="//goo.gl/jbehXd" target="_blank" rel="noopener">Go</a>
+            <a class="btn bt-enviar-contato center-block" href="https://goo.gl/jbehXd" target="_blank" rel="noopener">Open</a>
           </div>
         </div>
       </li>
@@ -49,12 +49,12 @@
             </div>
           </div>
           <div class="work-center work-center__nopadding">
-            <a class="" href="//goo.gl/zJW824" target="_blank" rel="noopener" title="Showcase Estúdio Udes - Portfolio João Paulo">
+            <a class="" href="https://goo.gl/zJW824" target="_blank" rel="noopener" title="Showcase Estúdio Udes - Portfolio João Paulo">
               <img class="img-responsive lazy" width="1000" height="594" data-original="assets/images/portfolio/site-udes2016.jpg" alt="Showcase Estúdio Udes - Portfolio João Paulo">
             </a>
           </div>
           <div class="work-center">
-            <a class="btn bt-enviar-contato center-block" href="//goo.gl/zJW824" target="_blank" rel="noopener">Go</a>
+            <a class="btn bt-enviar-contato center-block" href="https://goo.gl/zJW824" target="_blank" rel="noopener">Open</a>
           </div>
         </div>
       </li>
@@ -66,12 +66,12 @@
             </div>
           </div>
           <div class="work-center work-center__nopadding">
-            <a class="" href="//goo.gl/HS5gYY" target="_blank" rel="noopener" title="Showcase Colortil - Portfolio João Paulo">
+            <a class="" href="https://goo.gl/HS5gYY" target="_blank" rel="noopener" title="Showcase Colortil - Portfolio João Paulo">
               <img class="img-responsive lazy" width="1000" height="594" data-original="assets/images/portfolio/site-colortil2015.gif" alt="Showcase Colortil - Portfolio João Paulo">
             </a>
           </div>
           <div class="work-center">
-            <a class="btn bt-enviar-contato center-block" href="//goo.gl/HS5gYY" target="_blank" rel="noopener">Go</a>
+            <a class="btn bt-enviar-contato center-block" href="https://goo.gl/HS5gYY" target="_blank" rel="noopener">Open</a>
           </div>
         </div>
       </li>
@@ -83,12 +83,12 @@
             </div>
           </div>
           <div class="work-center work-center__nopadding">
-            <a class="" href="//goo.gl/r5GRiW" target="_blank" rel="noopener" title="Showcase Paradise Weekend | Camarote Sapucaí 2014 - Portfolio João Paulo">
+            <a class="" href="https://goo.gl/r5GRiW" target="_blank" rel="noopener" title="Showcase Paradise Weekend | Camarote Sapucaí 2014 - Portfolio João Paulo">
               <img class="img-responsive lazy" width="1000" height="594" data-original="assets/images/portfolio/site-fullpw2014-sapucai.png" alt="Showcase Paradise Weekend | Camarote Sapucaí 2014 - Portfolio João Paulo">
             </a>
           </div>
           <div class="work-center">
-            <a class="btn bt-enviar-contato center-block" href="//goo.gl/r5GRiW" target="_blank" rel="noopener">Go</a>
+            <a class="btn bt-enviar-contato center-block" href="https://goo.gl/r5GRiW" target="_blank" rel="noopener">Open</a>
           </div>
         </div>
       </li>
@@ -100,12 +100,12 @@
             </div>
           </div>
           <div class="work-center work-center__nopadding">
-            <a href="//goo.gl/r5GRiW" target="_blank" rel="noopener" title="Showcase Paradise Weekend | Camarote Sapucaí 2014 - Portfolio João Paulo">
+            <a href="https://goo.gl/r5GRiW" target="_blank" rel="noopener" title="Showcase Paradise Weekend | Camarote Sapucaí 2014 - Portfolio João Paulo">
               <img class="img-responsive lazy" width="1000" height="594" data-original="assets/images/portfolio/site-pw2014-sapucai.png" alt="">
             </a>
           </div>
           <div class="work-center">
-            <a class="btn bt-enviar-contato center-block" href="//goo.gl/r5GRiW" target="_blank" rel="noopener">Go</a>
+            <a class="btn bt-enviar-contato center-block" href="https://goo.gl/r5GRiW" target="_blank" rel="noopener">Open</a>
           </div>
         </div>
       </li>
@@ -117,12 +117,12 @@
             </div>
           </div>
           <div class="work-center work-center__nopadding">
-            <a href="//goo.gl/UtjvYv" target="_blank" rel="noopener" title="Showcase You Move – Curso de Inglês - Portfolio João Paulo">
+            <a href="https://goo.gl/UtjvYv" target="_blank" rel="noopener" title="Showcase You Move – Curso de Inglês - Portfolio João Paulo">
               <img  class="img-responsive lazy" width="1000" height="594" data-original="assets/images/portfolio/site-ymv2014.png" alt="Showcase You Move – Curso de Inglês - Portfolio João Paulo">
             </a>
           </div>
           <div class="work-center">
-            <a class="btn bt-enviar-contato center-block" href="//goo.gl/UtjvYv" target="_blank" rel="noopener">Go</a>
+            <a class="btn bt-enviar-contato center-block" href="https://goo.gl/UtjvYv" target="_blank" rel="noopener">Open</a>
           </div>
         </div>
       </li>
@@ -134,12 +134,12 @@
             </div>
           </div>
           <div class="work-center work-center__nopadding">
-            <a href="//goo.gl/dAchhu" target="_blank" rel="noopener" title="Showcase PARADISE WEEKEND - The Most Exclusive Festival 2013 - Portfolio João Paulo">
+            <a href="https://goo.gl/dAchhu" target="_blank" rel="noopener" title="Showcase PARADISE WEEKEND - The Most Exclusive Festival 2013 - Portfolio João Paulo">
               <img class="img-responsive lazy" width="1000" height="594" data-original="assets/images/portfolio/site-pw2013.png" alt="Showcase PARADISE WEEKEND - The Most Exclusive Festival 2013 - Portfolio João Paulo">
             </a>
           </div>
           <div class="work-center">
-            <a class="btn bt-enviar-contato center-block" href="//goo.gl/dAchhu" target="_blank" rel="noopener">Go</a>
+            <a class="btn bt-enviar-contato center-block" href="https://goo.gl/dAchhu" target="_blank" rel="noopener">Open</a>
           </div>
         </div>
       </li>
@@ -151,12 +151,12 @@
             </div>
           </div>
           <div class="work-center work-center__nopadding">
-            <a href="//goo.gl/fXxGDD" target="_blank" rel="noopener" title="Showcase SOUNDS of Maquinaria - Portfolio João Paulo">
+            <a href="https://goo.gl/fXxGDD" target="_blank" rel="noopener" title="Showcase SOUNDS of Maquinaria - Portfolio João Paulo">
               <img class="img-responsive lazy" width="1000" height="594" data-original="assets/images/portfolio/site-soundsofmaquinaria2012.png" alt="Showcase SOUNDS of Maquinaria - Portfolio João Paulo">
             </a>
           </div>
           <div class="work-center">
-            <a class="btn bt-enviar-contato center-block" href="//goo.gl/fXxGDD" target="_blank" rel="noopener">Go</a>
+            <a class="btn bt-enviar-contato center-block" href="https://goo.gl/fXxGDD" target="_blank" rel="noopener">Open</a>
           </div>
         </div>
       </li>
@@ -168,12 +168,12 @@
             </div>
           </div>
           <div class="work-center work-center__nopadding">
-            <a href="http://goo.gl/vUfKNd" target="_blank" rel="noopener" title="Showcase Ideioteca - Portfolio João Paulo">
+            <a href="https://goo.gl/vUfKNd" target="_blank" rel="noopener" title="Showcase Ideioteca - Portfolio João Paulo">
               <img class="img-responsive lazy" width="1000" height="594" data-original="assets/images/portfolio/site-ideioteca2012.png" alt="Showcase Ideioteca - Portfolio João Paulo">
             </a>
           </div>
           <div class="work-center">
-            <a class="btn bt-enviar-contato center-block" href="http://goo.gl/vUfKNd" target="_blank" rel="noopener">Go</a>
+            <a class="btn bt-enviar-contato center-block" href="https://goo.gl/vUfKNd" target="_blank" rel="noopener">Open</a>
           </div>
         </div>
       </li>
@@ -190,7 +190,7 @@
             <img class="img-responsive lazy" width="1000" height="594" data-original="assets/images/portfolio/card-karenteixeira.jpg" alt="">
           </div>
           <div class="work-center">
-            <a class="btn bt-enviar-contato center-block" href="assets/images/portfolio/card-karenteixeira.jpg" data-lightbox="image-1" data-title="Cartão de visita">Go</a>
+            <a class="btn bt-enviar-contato center-block" href="assets/images/portfolio/card-karenteixeira.jpg" data-lightbox="image-1" data-title="Cartão de visita">Open</a>
           </div>
         </div>
       </li>
@@ -200,7 +200,7 @@
             <img class="img-responsive lazy" width="1000" height="594" data-original="assets/images/portfolio/card-joaopaulo.jpg" alt="">
           </div>
           <div class="work-center">
-            <a class="btn bt-enviar-contato center-block"href="assets/images/portfolio/card-joaopaulo.jpg" data-lightbox="image-2" data-title="Cartão de visita">Go</a>
+            <a class="btn bt-enviar-contato center-block"href="assets/images/portfolio/card-joaopaulo.jpg" data-lightbox="image-2" data-title="Cartão de visita">Open</a>
           </div>
         </div>
       </li>
